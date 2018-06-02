0d560b03c3037ca4ce77b6454cb6824e18f46809
Giulia <teggi.giulia@gmail.com>
2018-06-02 14:57:50 +0100

    Create components folder and app component

---
+++ public/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+            <h1>CourtWatch</h1>
+            </React.Fragment>
+        )
+    }
+}
--- public/index.js
+++ public/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-const App = () => <h1>CourtWatch</h1>;
+import App from './components/app'; 
 
 render(<App />, document.getElementById('root'));
