3579d50c61f37bdc07bef52ed7b23dbdc05a33f8
Giulia <teggi.giulia@gmail.com>
2018-06-02 15:12:07 +0100

    Add navbar component

--- public/components/app.js
+++ public/components/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './navbar'; 
 
 export default class App extends React.Component {
     render() {
         return (
             <React.Fragment>
+                <Navbar />
             <h1>CourtWatch</h1>
             </React.Fragment>
         )
