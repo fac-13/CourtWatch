26a7cb4bd14f44eef3462272f1d5975159ffeb5d
HA <haydnba@gmail.com>
2018-06-03 02:47:29 +0100

    even more db/travis troubleshooting

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -2,20 +2,18 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-let { DATABASE_URL } = process.env;
-
-// if (process.env.TRAVIS === 'true') {
-//   DATABASE_URL = 'cwdb_test';
-// }
+let DB_URL;
 
 if (process.env.NODE_ENV === 'test') {
-  DATABASE_URL = process.env.TEST_DATABASE_URL;
+  DB_URL = process.env.TEST_DATABASE_URL;
+} else {
+  DB_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017';
 }
 
-if (!DATABASE_URL) {
+if (!DB_URL) {
   throw new Error('DATABASE_URL should be set');
 }
 
 module.exports = () => {
-  mongoose.connect(DATABASE_URL);
+  mongoose.connect(DB_URL);
 };
