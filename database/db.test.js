f222cab89b78a971e525a797b27c86c1125be7f2
HA <haydnba@gmail.com>
2018-06-03 01:04:40 +0100

    remove .travis.yml db settings - troubleshooting

--- database/db.test.js
+++ database/db.test.js
@@ -6,14 +6,14 @@ beforeAll(() => {
   dbconnection();
 });
 
+afterAll((done) => {
+  mongoose.disconnect(done);
+});
+
 test('Should fail when env not test ', () => {
   expect(process.env.NODE_ENV).toBe('test');
 });
 
 test('Test if database connection exists', () => {
   expect(mongoose.connection.name).toBe('cwdb_test');
-});
-
-afterAll((done) => {
-  mongoose.disconnect(done);
 });
