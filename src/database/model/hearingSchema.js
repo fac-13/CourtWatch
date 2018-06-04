95671701a7d74cb49c1aab361236e9502528bde4
Giulia <teggi.giulia@gmail.com>
2018-06-04 18:07:36 +0100

    Update hearing Schema

--- src/database/model/hearingSchema.js
+++ src/database/model/hearingSchema.js
@@ -1,27 +1,30 @@
 const mongoose = require('mongoose');
+const Court = require('./courtSchema');
+const Volunteer = require('./volunteerSchema');
 
 const { Schema } = mongoose;
 
+const contactSchema = new Schema({
+  _id: false,
+  name: String,
+  type: String,
+  email: String,
+  number: Number,
+});
+
 const HearingSchema = new Schema(
   {
-    date: Date,
-    court_id: { type: ObjectId, ref: Court }, // eslint-disable-line
+    date: String,
+    court_id: { type: Schema.Types.ObjectId, ref: Court }, // eslint-disable-line
     court_name: String,
     court_number: Number,
     watching: [
       {
         name: String,
-        volunteer_id: { type: ObjectId, ref: Volunteer }, // eslint-disable-line
+        volunteer_id: { type: Schema.Types.ObjectId, ref: Volunteer }, // eslint-disable-line
       },
     ],
-    contact: [
-      {
-        name: String,
-        type: String,
-        email: String,
-        number: Number,
-      },
-    ],
+    contact: [contactSchema],
   },
   { timestamps: true },
 );
