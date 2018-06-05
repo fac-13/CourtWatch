70d9867afccfbaea50b445d481c75b94d1806ddc
HA <haydnba@gmail.com>
2018-06-05 17:32:24 +0100

    update data helper function

--- src/data_helpers/populateAll.js
+++ src/data_helpers/populateAll.js
@@ -4,9 +4,9 @@
 const mongoose = require('mongoose');
 
 // import dump and/or dummy data
-const courts_dump = require('./courts_dump.json');
-const dummy_courts = require('./dummy_courts');
-const dummy_hearing = require('./dummy_hearing');
+// const courts_dump = require('./courts_dump.json');
+// const dummy_courts = require('./dummy_courts');
+// const dummy_hearing = require('./dummy_hearing');
 const dummy_volunteer = require('./dummy_volunteer');
 
 // import relevant schemas
@@ -17,8 +17,8 @@ const { Volunteer } = require('../database/model');
 // access environment variables
 require('dotenv').config();
 
-// configure andopen database connection
-const DATABASE_URL = process.env.MLAB_DATABASE_URL;
+// configure and open database connection
+const { DATABASE_URL } = process.env;
 if (!DATABASE_URL) {
   throw new Error('Environment variable DATABASE_URL should be set');
 }
@@ -44,11 +44,7 @@ function populateAll(schema, data) {
   });
 }
 
-populateAllCourts(court_dump.courts);
-
-module.exports = {
-  populateCourtsDump: populateAll(Court, courts_dump.courts),
-  populateCourts: populateAll(Court, dummy_courts.courts),
-  populateHearings: populateAll(Hearing, dummy_hearing),
-  populateVolunteers: populateAll(Volunteer, dummy_volunteer),
-};
+// populateAll(Court, courts_dump.courts);
+// populateAll(Court, dummy_courts.courts);
+// populateAll(Hearing, dummy_hearing);
+populateAll(Volunteer, dummy_volunteer);
