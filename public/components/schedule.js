2da403656bc92207e709f59dfcd83bd79c202e96
Giulia <teggi.giulia@gmail.com>
2018-06-05 13:13:55 +0100

    Add hearings data to state and render function of schedule.js Relates #28 #29

--- public/components/schedule.js
+++ public/components/schedule.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { getSchedule } from '../utils/fetch';
 
 export default class Schedule extends React.Component {
   state = {
-
+    data: null,
   }
 
   componentDidMount() {
-    fetch('/schedule-data')
-      .then(response => response.json())
-      .then((data) => { console.log(data); });
+    getSchedule('/schedule-data').then(hearings => this.setState({ data: hearings }));
   }
 
   render() {
     return (
-      <h1 > Schedule</h1 >
+      <React.Fragment>
+        <h1 > Schedule</h1>
+        {!this.state.data &&
+          <h3>Loading schedule...</h3>
+        }
+        {this.state.data &&
+          <article>
+            <p>{this.state.data[0].date}</p>
+            <p>{this.state.data[0].court_name}</p>
+          </article>
+        }
+      </React.Fragment>
     );
   }
 }
