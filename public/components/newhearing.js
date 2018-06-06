c925b37647ae0536315b3d65428d635d9c9c61df
Giulia <teggi.giulia@gmail.com>
2018-06-06 18:15:49 +0100

    Add onchange function to inputs

--- public/components/newhearing.js
+++ public/components/newhearing.js
@@ -22,20 +22,20 @@ export default class NewHearing extends React.Component {
           <form action="/add-hearing" method="post">
 
             <label htmlFor="date">Date:</label>
-            <input list="date" name="date" id="name" value={this.state.date} />
+            <input list="date" name="date" id="name" value={this.state.date} onChange={this.handleChange} />
             <datalist id="date">
               <option value="hello" />
               <option value="Ciao" />
             </datalist>
 
             <label htmlFor="court">Court:</label>
-            <input list="text" name="court" id="court" value={this.state.court} />
+            <input list="text" name="court" id="court" value={this.state.court} onChange={this.handleChange} />
 
             <h4>Contact details (optional)</h4>
             <label htmlFor="name">Name:</label>
-            <input type="text" name="text" value={this.state.name} />
-            <p>I am a...</p>
-            <input list="type" name="type" value={this.state.type} />
+            <input type="text" name="text" id="name" value={this.state.name} onChange={this.handleChange} />
+            <label htmlFor="type">I am a...</label>
+            <input list="type" name="type" id="type" value={this.state.type} onChange={this.handleChange} />
             <datalist id="type">
               <option value="Solicitor" />
               <option value="Social worker" />
@@ -44,10 +44,10 @@ export default class NewHearing extends React.Component {
             </datalist>
 
             <label htmlFor="email">Email:</label>
-            <input type="text" name="email" value={this.state.email} />
+            <input type="text" name="email" id="email" value={this.state.email} onChange={this.handleChange} />
 
             <label htmlFor="phone">Phone number:</label>
-            <input type="text" name="number" value={this.state.phone} />
+            <input type="text" name="number" id="phone" value={this.state.phone} onChange={this.handleChange} />
             <button type="submit"><Link to="/schedule">Add hearing</Link></button>
           </form>
         </section>
