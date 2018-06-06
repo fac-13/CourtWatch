c257e5fb45fd0ed4a56a4e36d27379115c2eec6d
Giulia <teggi.giulia@gmail.com>
2018-06-06 17:51:57 +0100

    Add state to newhearing component

--- public/components/newhearing.js
+++ public/components/newhearing.js
@@ -2,43 +2,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NewHearing = () => (
-  <React.Fragment>
-    <h1>Add a new hearing</h1>
-    <section>
-      <form action="/add-hearing" method="post">
+export default class NewHearing extends React.Component {
+  state = {
+    date: null,
+    court: null,
+    name: null,
+    email: null,
 
-        <label htmlFor="date">Date:</label>
-        <input list="date" name="date" id="name" />
-        <datalist id="date">
-          <option value="hello" />
-          <option value="Ciao" />
-        </datalist>
+  }
 
-        <label htmlFor="court">Court:</label>
-        <input list="text" name="court" id="court" />
+  render() {
+    return (
+      < React.Fragment >
+        <h1>Add a new hearing</h1>
+        <section>
+          <form action="/add-hearing" method="post">
 
-        <h4>Contact details (optional)</h4>
-        <label htmlFor="name">Name:</label>
-        <input type="text" name="text" />
-        <p>I am a...</p>
-        <input list="type" name="type" />
-        <datalist id="type">
-          <option value="Solicitor" />
-          <option value="Social worker" />
-          <option value="Defendant" />
-          <option value="Other" />
-        </datalist>
+            <label htmlFor="date">Date:</label>
+            <input list="date" name="date" id="name" />
+            <datalist id="date">
+              <option value="hello" />
+              <option value="Ciao" />
+            </datalist>
 
-        <label htmlFor="email">Email:</label>
-        <input type="text" name="email" />
+            <label htmlFor="court">Court:</label>
+            <input list="text" name="court" id="court" />
 
-        <label htmlFor="phone">Phone number:</label>
-        <input type="text" name="number" />
-        <button type="submit"><Link to="/schedule">Add hearing</Link></button>
-      </form>
-    </section>
-  </React.Fragment>
-);
+            <h4>Contact details (optional)</h4>
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="text" />
+            <p>I am a...</p>
+            <input list="type" name="type" />
+            <datalist id="type">
+              <option value="Solicitor" />
+              <option value="Social worker" />
+              <option value="Defendant" />
+              <option value="Other" />
+            </datalist>
+
+            <label htmlFor="email">Email:</label>
+            <input type="text" name="email" />
+
+            <label htmlFor="phone">Phone number:</label>
+            <input type="text" name="number" />
+            <button type="submit"><Link to="/schedule">Add hearing</Link></button>
+          </form>
+        </section>
+      </React.Fragment >
+    )
+  }
+};
 
 export default NewHearing;
