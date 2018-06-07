1828846afec2fff599e51d092bcb8a47e567115a
HA <haydnba@gmail.com>
2018-06-07 11:26:10 +0100

    update comments on court data fn and remove null imports in model index

--- src/database/model/index.js
+++ src/database/model/index.js
@@ -1,11 +1,9 @@
 const { Court } = require('./courtSchema');
 const { Hearing } = require('./hearingSchema');
 const { Volunteer } = require('./volunteerSchema');
-const { Simple } = require('./simpleSchema');
 
 module.exports = {
   Court,
   Hearing,
   Volunteer,
-  Simple,
 };
