d7c87b08d55bb7e0cd5771e9fb1479ec01c4a8fd
HA <haydnba@gmail.com>
2018-06-07 12:15:29 +0100

    Revert "basic component rendering for schedule data"
    This reverts commit c7f0dfd3b07769ff57e59d9c8eeecf81d73dd794.

--- public/components/listhearings.js
+++ public/components/listhearings.js
@@ -2,47 +2,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
-import { getData } from '../utils/fetch';
 
-export default class ListHearings extends React.Component {
-  state = {
-    data: null,
-  };
+const ListHearings = (props) => {
+  const { hearings } = props;
 
-  componentDidMount() {
-    getData('/schedule-data').then(hearings =>
-      this.setState({ data: hearings }, () => console.log(hearings)),
-    );
-  }
-
-  //generate calendar rows
-  twoWeeks = () => {
+  //generate calendar rows 
+  const TwoWeeks = () => {
     let start = moment().subtract(1, 'days');
     const arr = Array.from({ length: 14 });
-    return arr.map((item, index) => {
-      // const dayOfWeek = start.add(1, 'days').format('dddd DD');
-      <article key={index}>
-        <section>{start.add(1, 'days').format('dddd DD')}</section>
-        <section>
-          <p>HaydnGiulia</p>
-        </section>
-      </article>;
-    });
-  };
-
-  // MonthYear = () => {
-  //   let date = moment().format('MMMM YYYY');
-  //   return [date];
-  // };
-
-  render() {
-    return (
-      <main>
-        {/* <header>
-          <MonthYear />
-        </header> */}
-        <section>{this.twoWeeks}</section>
-      </main>
-    );
+    const result = arr.map((item, index) => <tr key={index}>{start.add(1, 'days').format("dddd DD")}</tr>);
+    return result;
   }
-}
+
+  return (
+    <table>
+      <thead>
+        <Month />
+      </thead>
+      <tbody>
+        <tr>
+          <TwoWeeks />
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default ListHearings;
