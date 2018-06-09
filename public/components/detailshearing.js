d2e4b249fbe2f94939befab39426c8791ef16a79
Giulia <teggi.giulia@gmail.com>
2018-06-09 13:47:02 +0100

    Format date shown on hearing details page with moment.js

--- public/components/detailshearing.js
+++ public/components/detailshearing.js
@@ -1,26 +1,34 @@
 /* eslint-disable */
 
 import React from 'react';
+import moment from 'moment';
+import Button from './button';
 
 const DetailsHearings = (props) => {
   const { hearing } = props;
   const { addresses, contact } = hearing[0];
+
+  //Changes format of date 
+  const date = moment(hearing[0].date).format('dddd D MMMM YYYY')
 
   // Regex and filter function to render only address for visits (not postal address)
   const regex = /visit/i;
   const address = addresses.filter(item => regex.test(item.type) === true);
 
   return (
-    <section>
-      <h4>Date:</h4>
-      <p>{hearing[0].date}</p>
+    <section className="hearing_details">
+      <Button link="" text="Attend" />
+      <h4> Date:</h4>
+      <p>{date}</p>
       <h4>Court</h4>
       <p>{hearing[0].court_name}</p>
       <h4>Address:</h4>
       <p>{address[0].address}</p>
       <p>{address[0].town}</p>
       <p>{address[0].county}</p>
-    </section>
+      <h4>CourtWatchers attending the hearing:</h4>
+      <p>No CourtWatchers are booked to attend the hearing</p>
+    </section >
   );
 };
 
