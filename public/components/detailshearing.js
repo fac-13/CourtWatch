2c1cd75166481f9aa2f9916e8edfbdf83cbce096
Giulia <teggi.giulia@gmail.com>
2018-06-09 19:27:45 +0100

    Add style to detail schedule view

--- public/components/detailshearing.js
+++ public/components/detailshearing.js
@@ -8,19 +8,16 @@ const DetailsHearings = (props) => {
   const { hearing } = props;
   const { addresses, contact } = hearing[0];
 
-  //Changes format of date 
+  //Change format of date 
   const date = moment(hearing[0].date).format('dddd D MMMM YYYY')
 
   // Regex and filter function to render only address for visits (not postal address)
   const regex = /visit/i;
-  const addresses = addresses.filter(item => regex.test(item.type) === true);
+  const addressBlock = addresses.filter(item => regex.test(item.type) === true);
 
-  //Replace \n with <br> to go on new line
+  //replace new line symbol with break
   const Address = () => {
-    addressBlock[0].address.split('\n').map((item, key) => {
-      console.log("Item", item)
-      return <span key={key}>{item}<br /></span>
-    })
+    return addressBlock[0].address.split('\n').map((item, key) => <span key={key}>{item}<br /></span>);
   }
 
   return (
@@ -31,7 +28,7 @@ const DetailsHearings = (props) => {
           <h4> Date:</h4>
         </section>
         <section className="hearing_right_column">
-          <p>{date}</p>
+          <span>{date}</span>
         </section>
       </section>
 
@@ -40,7 +37,7 @@ const DetailsHearings = (props) => {
           <h4>Court:</h4>
         </section>
         <section className="hearing_right_column">
-          <p>{hearing[0].court_name}</p>
+          <span>{hearing[0].court_name}</span>
         </section>
       </section>
 
@@ -50,20 +47,25 @@ const DetailsHearings = (props) => {
         </section>
         <section className="hearing_right_column">
           <Address />
-          <p>{addresses[0].town}</p>
-          <p>{addresses[0].county}</p>
-          <p>{addresses[0].postcode}</p>
+          <span>{addressBlock[0].town}<br /></span>
+          <span>{addressBlock[0].county}</span>
         </section>
       </section>
 
-      <Button className="hearing_section" link="/schedule" text="Attend" />
-
       <section className="hearing_section">
+        <section className="hearing_left_column">
+          <h4>Postcode:</h4>
+        </section>
+        <section className="hearing_right_column">
+          <span>{addressBlock[0].postcode}</span>
+        </section>
+      </section>
+      <Button className="hearing_button" link="/schedule" text="Attend" />
+      <section>
         <h4>CourtWatchers attending the hearing:</h4>
         <p>No CourtWatchers are booked to attend the hearing</p>
       </section>
-
-    </article>
+    </article >
   );
 };
 
