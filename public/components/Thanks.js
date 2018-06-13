485b960f3761445ec7f7beb6655417f4cd3843ed
Giulia <teggi.giulia@gmail.com>
2018-06-13 15:47:42 +0100

    Thanks

--- public/components/Thanks.js
+++ public/components/Thanks.js
@@ -2,16 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Thanks = () => (
-    <React.Fragment>
-      <h1>Thank you!</h1>
-      <main className="main">
-
-        <h4>The hearing has been successfully added to our database.</h4>
-        <p>If you want to have a look at all the upcoming hearings, please go to the <Link to='/schedule' className="link">schedule page</Link>.</p>
-
-
-      </main>
-    </React.Fragment>
-  );
+  <React.Fragment>
+    <h1>Thank you!</h1>
+    <main className="main">
+      <h4>The hearing has been successfully added to our database.</h4>
+      <p>If you want to have a look at all the upcoming hearings, please go to the <Link to="/schedule" className="link">schedule page</Link>.</p>
+    </main>
+  </React.Fragment>
+);
 
 export default Thanks;
