965b6d2847a34ed8bf7b98bb00c637ae238a4e96
Giulia <teggi.giulia@gmail.com>
2018-06-26 14:26:35 +0100

    Update style of homeapage

--- public/components/Home.js
+++ public/components/Home.js
@@ -12,18 +12,15 @@ const Home = () => {
         <section className="home_title">
           <h1>CourtWatch</h1>
         </section>
-        <section className="home_section text">
-          <div className="first_div">
-            <h4>Every woman has a right to a fair trial.</h4><p>Yet many times crucial mitigating circumstances are not taken into account by the justice system.</p>
-            <h4>Unfair prison sentences break the lives of women and their families.</h4>
-          </div>
-          <div className="second_div">
-            <p><Link to="/join" className="link">Join CourtWatch</Link> to monitor and report on civil and criminal court cases, so to defend the right of every woman to receive a fair and impartial trial.</p>
-            <section className="home_section buttons">
-              <Button className="home_button" link={join} text="Join CourtWatch" />
-              <Button className="home_button" link={addHearing} text="Add a hearing" />
-            </section>
-          </div>
+        <section className="first_section">
+          <h4>Every woman has a right to a fair trial.</h4>
+        </section>
+        <section className="second_section">
+          <h4><Link to="/join" className="link">Join CourtWatch</Link> to monitor and report on civil and criminal court cases, so to defend the right of every woman to receive a fair and impartial trial.</h4>
+        </section>
+        <section className="third_section">
+          <Button className="home_button" link={join} text="Become a volunteer" />
+          <Button className="home_button" link={addHearing} text="Add a hearing" />
         </section>
       </section>
     </React.Fragment>
